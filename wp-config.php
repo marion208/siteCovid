51b7d53748f09b182879903a3fc384743ddbfd74
Marion Marlois <marion.marlois@gmail.com>
2021-04-06 18:06:01 +0200

    update database login

--- wp-config.php
+++ wp-config.php
@@ -38,7 +38,7 @@ define('DB_USER', 'wpUser');
 define('DB_PASSWORD', 'A67Qr%NF8nkepqrTp');
 
 /** Adresse de l’hébergement MySQL. */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define('DB_CHARSET', 'utf8mb4');
